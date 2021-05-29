88b8fcd80fb33bfcba8c92aa21cb01d067408541
Noel De Martin <noeldemartin@gmail.com>
2021-05-29 11:03:27 +0200

    Make Constructor type parameters optional

--- src/models/inference.ts
+++ src/models/inference.ts
@@ -3,6 +3,6 @@ import type { Constructor } from '@noeldemartin/utils';
 import type { SolidModel } from './SolidModel';
 import type SolidContainerModel from './SolidContainerModel';
 
-export type SolidModelConstructor<T extends SolidModel> = Constructor<T> & typeof SolidModel;
-export type SolidContainerModelConstructor<T extends SolidContainerModel> =
+export type SolidModelConstructor<T extends SolidModel = SolidModel> = Constructor<T> & typeof SolidModel;
+export type SolidContainerModelConstructor<T extends SolidContainerModel = SolidContainerModel> =
     SolidModelConstructor<T> & typeof SolidContainerModel;
