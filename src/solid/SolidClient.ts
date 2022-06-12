8e503871e56346be0a9b03cf9cf17f92b07cb04a
Noel De Martin <noel@noeldemartin.com>
2022-06-12 14:44:45 +0200

    Fix overwrite request

--- src/solid/SolidClient.ts
+++ src/solid/SolidClient.ts
@@ -170,7 +170,6 @@ export default class SolidClient {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/sparql-update',
-                'If-None-Match': '*',
             },
             body: `
                 DELETE DATA { ${RDFResourceProperty.toTurtle(document.properties, url)} } ;
