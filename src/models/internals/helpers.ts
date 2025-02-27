e9111a15745f329f59133f22f5a1fbcb43eb6198
Noel De Martin <noel@noeldemartin.com>
2025-02-27 17:47:36 +0100

    Implement bootSolidSchema helper

--- src/models/internals/helpers.ts
+++ src/models/internals/helpers.ts
@@ -1,12 +1,18 @@
+import { fail } from '@noeldemartin/utils';
 import { SoukaiError, defineModelSchema } from 'soukai';
-import type { SchemaDefinition } from 'soukai';
 import type { Constructor } from '@noeldemartin/utils';
+import type { SchemaDefinition } from 'soukai';
 
-import type { RDFContexts } from '@/models/fields';
+import type { RDFContexts, SolidSchemaDefinition } from '@/models/fields';
 import type { SolidMagicAttributes, SolidModelConstructor } from '@/models/inference';
 import type { SolidModel } from '@/models/SolidModel';
 
 const ongoingUpdates = new WeakMap<SolidModelConstructor, RDFContexts>();
+const bootedSchemas = new WeakMap<SolidSchemaDefinition, SolidModelConstructor>();
+
+function isModelClass(schema: SolidSchemaDefinition | SolidModelConstructor): schema is SolidModelConstructor {
+    return Object.getPrototypeOf(schema)?.constructor !== Object;
+}
 
 export function startSchemaUpdate(modelClass: SolidModelConstructor, context: RDFContexts): void {
     if (ongoingUpdates.has(modelClass)) {
@@ -24,6 +30,31 @@ export function getSchemaUpdateContext(modelClass: SolidModelConstructor): RDFCo
 
 export function stopSchemaUpdate(modelClass: SolidModelConstructor): void {
     ongoingUpdates.delete(modelClass);
+}
+
+export function bootSolidSchemaDecoupled<BaseModel extends SolidModel, Schema extends SchemaDefinition>(
+    baseModelOrDefinition: SolidModelConstructor<BaseModel> | Schema,
+    defaultModel: SolidModelConstructor,
+    definition?: Schema,
+): SolidModelConstructor{
+    const baseModel = definition ? baseModelOrDefinition as SolidModelConstructor : defaultModel;
+    const schema = definition ?? baseModelOrDefinition;
+
+    if (isModelClass(schema)) {
+        schema.ensureBooted();
+
+        return schema;
+    }
+
+    if (!bootedSchemas.has(schema)) {
+        const bootedSchema = defineSolidModelSchemaDecoupled(schema, baseModel);
+
+        bootedSchema.ensureBooted();
+
+        bootedSchemas.set(schema, bootedSchema);
+    }
+
+    return bootedSchemas.get(schema) ?? fail('Couldn\'t boot schema');
 }
 
 export function defineSolidModelSchemaDecoupled<BaseModel extends SolidModel, Schema extends SchemaDefinition>(
--- src/models/schema.ts
+++ src/models/schema.ts
@@ -1,9 +1,21 @@
 import type { Constructor } from '@noeldemartin/utils';
 
-import { defineSolidModelSchemaDecoupled } from './internals/helpers';
+import { bootSolidSchemaDecoupled, defineSolidModelSchemaDecoupled } from './internals/helpers';
 import { SolidModel } from './SolidModel';
 import type { SolidMagicAttributes, SolidModelConstructor } from './inference';
 import type { SolidSchemaDefinition } from './fields';
+
+/* eslint-disable max-len */
+export function bootSolidSchema<Schema extends SolidSchemaDefinition>(definition: Schema): Constructor<SolidMagicAttributes<Schema>> & SolidModelConstructor;
+export function bootSolidSchema<BaseModel extends SolidModel, Schema extends SolidSchemaDefinition>(baseModel: SolidModelConstructor<BaseModel>, definition: Schema): Constructor<SolidMagicAttributes<Schema>> & SolidModelConstructor<BaseModel>;
+/* eslint-enable max-len */
+
+export function bootSolidSchema<BaseModel extends SolidModel, Schema extends SolidSchemaDefinition>(
+    baseModelOrDefinition: SolidModelConstructor<BaseModel> | Schema,
+    definition?: Schema,
+): SolidModelConstructor {
+    return bootSolidSchemaDecoupled(baseModelOrDefinition, SolidModel, definition);
+}
 
 /* eslint-disable max-len */
 export function defineSolidModelSchema<Schema extends SolidSchemaDefinition>(definition: Schema): Constructor<SolidMagicAttributes<Schema>> & SolidModelConstructor;
