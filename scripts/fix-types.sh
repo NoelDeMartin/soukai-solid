72025fe4320cd9c043aca3885d21d233dd90e8a4
Noel De Martin <noel@noeldemartin.com>
2025-03-24 13:27:44 +0100

    Fix types, take 2

--- scripts/fix-types.sh
+++ scripts/fix-types.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-sed -i 's/SolidModel_2/SolidModel/g' "dist/soukai-solid.d.ts"
-sed -i 's/SolidModel_3/SolidModel/g' "dist/soukai-solid.d.ts"
-sed -i '/^import { SolidModel as SolidModel } from/d' "dist/soukai-solid.d.ts"
+sed -i 's/\& SolidModel_2/\& typeof SolidModel/g' "dist/soukai-solid.d.ts"
+sed -i 's/\& SolidModel_3/\& typeof SolidModel/g' "dist/soukai-solid.d.ts"
+sed -i 's/\Constructor<SolidModel_2>/\Constructor<SolidModel>/g' "dist/soukai-solid.d.ts"
+sed -i 's/\Constructor<SolidModel_3>/\Constructor<SolidModel>/g' "dist/soukai-solid.d.ts"
+sed -i '/^import { SolidModel as SolidModel_2 } from/d' "dist/soukai-solid.d.ts"
+sed -i '/^import { SolidModel as SolidModel_3 } from/d' "dist/soukai-solid.d.ts"
