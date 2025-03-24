25e19aa8919ad05ab3d2b5e224654aee15778fc2
Noel De Martin <noel@noeldemartin.com>
2025-03-24 15:47:55 +0100

    Update vitest types

--- src/types/vitest.d.ts
+++
@@ -1,6 +0,0 @@
-import type { VitestSolidMatchers } from '@noeldemartin/solid-utils/testing';
-
-declare module 'vitest' {
-    interface Assertion<T> extends VitestSolidMatchers<T> {}
-    interface AsymmetricMatchersContaining extends VitestSolidMatchers {}
-}
